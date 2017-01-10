a8fef81c8e5fc975031220b5a65ac6ff2bb15290
Berenger Bramas <bbramas@mpcdf.mpg.de>
2017-01-10 16:10:50 +0100

    Add Flops class and Flops example to compute flop easily

--- UTests/test-flops.cpp
+++ UTests/test-flops.cpp
@@ -7,17 +7,13 @@
 #include "@TYPE@/InaVec@TYPE@Double.hpp"
 #include "@TYPE@/InaVec@TYPE@Float.hpp"
 
-#include "core-test-all.hpp"
+#include "flops-test-all.hpp"
 #include "FLOPS/InaVecFLOPS.hpp"
 
 int main() {
     // clang-format off
-    TestAll<InaVec@TYPE@<double>> testerDouble;
-    TestAll<InaVec@TYPE@<float>> testerSingle;
-
-    TestAll<InaVecFLOPS<InaVec@TYPE@<double>>> testerDoubleFlops;
-    TestAll<InaVecFLOPS<InaVec@TYPE@<float>>> testerSingleFlops;
+    FlopsTestAll<InaVecFLOPS<InaVec@TYPE@<double>>> testerDoubleFlops;
+    FlopsTestAll<InaVecFLOPS<InaVec@TYPE@<float>>> testerSingleFlops;
     // clang-format on
-    return testerDouble.Run() + testerSingle.Run()
-            + testerDoubleFlops.Run() + testerSingleFlops.Run();
+    return testerDoubleFlops.Run() + testerSingleFlops.Run();
 }
