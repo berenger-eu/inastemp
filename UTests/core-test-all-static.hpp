6e23af9424396e810c3d8b711e9afce251cf9cbe
Berenger Bramas <Berenger.Bramas@inria.fr>
2020-05-09 14:52:22 +0200

    Continue debug

--- UTests/core-test-all-static.hpp
+++ UTests/core-test-all-static.hpp
@@ -482,8 +482,8 @@ class TestAll : public UTester< TestAll< VecType > > {
             // TODO approxEqualToArray(VecType(reals).exp(), expres);
             // TODO approxLowAccEqualToArray(VecType(reals).expLowAcc(), expreslowacc);
             approxEqualToArray(VecType(reals).sqrt(), sqrtres);
-            return; // TODO
             approxEqualToArray(VecType(reals).rsqrt(), rsqrtres);
+            return; // TODO
 
             approxEqualToScalar(VecType(RealType(0)).exp(), std::exp(RealType(0)));
         }
