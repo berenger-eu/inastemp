e4e6f3f1e14133e9c71679ce254d191816320c5e
Berenger Bramas <Berenger.Bramas@inria.fr>
2020-05-09 13:54:52 +0200

    Continue debug

--- Src/SXA/InaVecSXAFloat.hpp
+++ Src/SXA/InaVecSXAFloat.hpp
@@ -235,7 +235,7 @@ public:
         //__vr offset = _vel_vldu_vssl(4, inIndirection, 256);
         //vec = _vel_vldlsx_vssvl(0, values, offset, 256);
 
-        __vr offset = _vel_vld_vssl(4, inIndirection, 256);
+        __vr offset = _vel_vld_vssl(8, inIndirection, 256);
         __vr address = _vel_vsfa_vvssl(offset, 3, reinterpret_cast<unsigned long>(values), 256);
         vec = _vel_vgtu_vvssl(address, 0, 0, 256);
 
