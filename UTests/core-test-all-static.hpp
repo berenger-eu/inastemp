ab03b9677237f76820ff7ddce44255870bb83ccc
Berenger Bramas <Berenger.Bramas@inria.fr>
2020-05-09 13:59:52 +0200

    Continue debug

--- UTests/core-test-all-static.hpp
+++ UTests/core-test-all-static.hpp
@@ -422,7 +422,6 @@ class TestAll : public UTester< TestAll< VecType > > {
             UASSERTEEQUAL(VecType(10).horizontalMul(), RealType(pow(10, VecType::GetVecLength())));
         }
         printf("LINE %d\n", __LINE__);
-        return ; // TODO
 
         {
             equalToScalar(VecType::Min(VecType(1),
@@ -445,6 +444,7 @@ class TestAll : public UTester< TestAll< VecType > > {
             equalToScalar(VecType(RealType(0)).abs(), RealType(0));
         }
         printf("LINE %d\n", __LINE__);
+        return ; // TODO
 
         {
             RealType reals[VecType::GetVecLength()];
